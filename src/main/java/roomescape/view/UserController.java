31021f3edee3813ab1eee1fd2fdfb18c0e6f6445
julie <7jujubuilds@gmail.com>
2025-07-10 21:56:56 +0900

    ODSay API test

--- src/main/java/roomescape/view/UserController.java
+++ src/main/java/roomescape/view/UserController.java
@@ -1,5 +1,12 @@
 package roomescape.view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import roomescape.global.auth.Auth;
@@ -7,6 +14,9 @@ import roomescape.member.domain.Role;
 
 @Controller
 public class UserController {
+
+    @Value("${odsay.secret}")
+    private String odsay;
 
     @Auth(Role.USER)
     @GetMapping("/reservation")
@@ -31,8 +41,31 @@ public class UserController {
 
     @Auth(Role.GUEST)
     @GetMapping("/")
-    public String index(
-    ) {
+    public String index() throws IOException {
+        String apiKey = odsay;
+
+        String urlInfo = "https://api.odsay.com/v1/api/searchPubTransPathT?SX=126.9027279&SY=37.5349277&EX=126.9145430&EY=37.5499421&apiKey=" + URLEncoder.encode(apiKey, "UTF-8");
+
+        // http 연결
+        URL url = new URL(urlInfo);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+
+        BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line);
+        }
+        bufferedReader.close();
+        conn.disconnect();
+
+        // 결과 출력
+        System.out.println(sb.toString());
+
         return "index";
     }
 
